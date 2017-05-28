a0032f6db2af26d5d0461dbac5b40a441d12d265
chck <candysynd@gmail.com>
2017-05-29 00:33:07 +0900

    [add] spider for chiebukuro

--- gimme_wisdom/items.py
+++ gimme_wisdom/items.py
@@ -10,5 +10,4 @@ import scrapy
 
 class GimmeWisdomItem(scrapy.Item):
     # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+    answer = scrapy.Field()
--- gimme_wisdom/spiders/chiebukuro.py
+++ gimme_wisdom/spiders/chiebukuro.py
@@ -1,11 +1,40 @@
 # -*- coding: utf-8 -*-
 import scrapy
+from bs4 import BeautifulSoup
+
+from gimme_wisdom.items import GimmeWisdomItem
 
 
 class ChiebukuroSpider(scrapy.Spider):
     name = 'chiebukuro'
     allowed_domains = ['detail.chiebukuro.yahoo.co.jp']
-    start_urls = ['http://detail.chiebukuro.yahoo.co.jp/']
+    base_url = 'http://detail.chiebukuro.yahoo.co.jp/qa/question_detail'
+
+    def __init__(self, page_id, *args, **kwargs):
+        super(ChiebukuroSpider, self).__init__(*args, **kwargs)
+        self.page_id = page_id
+        self.start_urls = [
+            '{}/{}'.format(self.base_url, page_id)
+        ]
 
     def parse(self, response):
-        pass
+        soup = BeautifulSoup(response.body, 'lxml')
+
+        # best answer is in only first page
+        if '?page=' not in response.url:
+            best_answer = soup.find('div', class_='mdPstd mdPstdBA othrAns clrfx').find('p', 'queTxt').text
+            yield GimmeWisdomItem(answer=best_answer)
+
+        # other answer
+        yield scrapy.Request(response.url, callback=self.parse_answer)
+        next_page = soup.find('div', class_='mdPager').find('a', class_='aft')
+        if not next_page:
+            yield
+        else:
+            yield scrapy.Request(next_page['href'])
+
+    def parse_answer(self, response):
+        soup = BeautifulSoup(response.body, 'lxml')
+        for div in soup.find_all('div', class_='othrAns clrfx'):
+            answer = div.find('p', class_='queTxt').text
+            yield GimmeWisdomItem(answer=answer)
