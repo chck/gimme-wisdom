52c3c3fa668b707042872fbabe1a595799ed1f61
chck <iwazaki_yuki@cyberagent.co.jp>
2017-06-01 18:16:42 +0900

    [add] type to item

--- gimme_wisdom/items.py
+++ gimme_wisdom/items.py
@@ -11,12 +11,12 @@ import scrapy
 class QItem(scrapy.Item):
     """no question one answer
     """
-    answer = scrapy.Field()
+    answer: str = scrapy.Field()
 
 
 class QandAsItem(scrapy.Item):
     """one question many answers
     """
-    answers = scrapy.Field()
-    question = scrapy.Field()
-    link = scrapy.Field()
+    answers: list = scrapy.Field()
+    question: str = scrapy.Field()
+    link: str = scrapy.Field()
